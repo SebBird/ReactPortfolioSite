b922354767c5b920ec125ba4ef6a83c3ce78445a
Seb <sebbird95@gmail.com>
2021-05-04 12:15:29 +0100

    add footer, fixed menu on mobile

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import linkedin from "../Assets/linkedin.png";
+import github from "../Assets/github.png";
+import instagram from "../Assets/instagram.png";
+
+const MainFooter = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: rgba(53, 43, 43, 1);
+  height: 120px;
+`;
+
+const SocialImage = styled.img`
+  height: 30px;
+  padding: 0.5rem;
+  transition: 0.5s;
+  &:hover {
+    filter: invert(1);
+  }
+`;
+
+const Footer = () => {
+  return (
+    <MainFooter>
+      <p>Built with React</p>
+      <div>
+        <a href="">
+          <SocialImage src={linkedin} alt="" />
+        </a>
+        <a href="https://github.com/SebBird" target="_blank">
+          <SocialImage src={github} alt="GitHub" />
+        </a>
+        <a href="">
+          <SocialImage src={instagram} alt="" />
+        </a>
+      </div>
+    </MainFooter>
+  );
+};
+
+export default Footer;
