a2a06b6defb77e55cd3e0c29b6c315dadba329e5
SebBird <80720407+SebBird@users.noreply.github.com>
2025-02-06 20:59:37 +0000

    Update Frog.jsx

--- src/Components/Frog.jsx
+++ src/Components/Frog.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FrogPic from "../Assets/frog.png";
 
 const Frog = () => 
-    <a href="https://froggie.netlify.app/" rel="noreferrer" target="_blank">
+    <a href="https://sbsurprise.netlify.app/" rel="noreferrer" target="_blank">
         <img src={FrogPic} alt="HiddenFrog" />
     </a>
 
